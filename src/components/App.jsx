5666b2dbab1921625d8d5c0a78e979c6813cba4f
EvgenS-it <evgens.it101@gmail.com>
2024-08-29 17:27:45 +0100

    comments deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,6 @@
 import { lazy, Suspense } from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
-
-// import HomePage from '../pages/HomePage/HomePage.jsx';
-// import MoviesPage from '../pages/MoviesPage/MoviesPage.jsx';
-// import MovieDetailsPage from '../pages/MovieDetailsPage/MovieDetailsPage.jsx';
-// import NotFoundPage from '../pages/NotFoundPage/NotFoundPage.jsx';
-
-// import Navigation from './Navigation/Navigation.jsx';
-// import MovieCast from './MovieCast/MovieCast.jsx';
-// import MovieReviews from './MovieReviews/MovieReviews.jsx';
+import './App.css';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage.jsx'));
 const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage.jsx'));
